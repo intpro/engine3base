a14fbacd9c61952d2199ee28ae24a54538b1c284
InterProFront <fallzem@gmail.com>
2017-05-12 10:10:27 +0600

    fix front layout and add file type

--- config/app.php
+++ config/app.php
@@ -151,31 +151,32 @@ return [
 
         //===================interpro=================
         Interpro\Core\CoreTypesServiceProvider::class,
-        //--------------------------------------------
+        //------------------------------------------—
         Interpro\Scalar\ScalarFirstServiceProvider::class,
         Interpro\Seo\SeoFirstServiceProvider::class,
         Interpro\ImageAggr\ImageAggrFirstServiceProvider::class,
+        Interpro\FileAggr\FileAggrFirstServiceProvider::class,
         Interpro\QS\QSFirstServiceProvider::class,
         Interpro\Feedback\FeedbackFirstServiceProvider::class,
         //============================================
-
         //============================================
         Interpro\Core\CoreExecServiceProvider::class,
         Interpro\Extractor\ExtractorServiceProvider::class,
         Interpro\Service\ServiceServiceProvider::class,
-        //--------------------------------------------
+        //------------------------------------------—
         Interpro\Scalar\ScalarSecondServiceProvider::class,
         Interpro\Seo\SeoSecondServiceProvider::class,
         Interpro\ImageAggr\ImageAggrSecondServiceProvider::class,
+        Interpro\FileAggr\FileAggrSecondServiceProvider::class,
         Interpro\QS\QSSecondServiceProvider::class,
         Interpro\Feedback\FeedbackSecondServiceProvider::class,
         //============================================
-
         //============================================
         Interpro\Core\CoreTaxonomyServiceProvider::class,
         Interpro\Entrance\EntranceServiceProvider::class,
-        //--------------------------------------------
+        //------------------------------------------—
         Interpro\ImageAggr\ImageAggrUseServiceProvider::class,
+        Interpro\FileAggr\FileAggrUseServiceProvider::class,
         Interpro\Feedback\FeedbackUseServiceProvider::class,
         //============================================
 
