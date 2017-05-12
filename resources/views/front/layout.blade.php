59631b46c8b228f2c7f20ae67f91a4307da700fc
InterProFront <fallzem@gmail.com>
2017-05-12 10:12:37 +0600

    fix front viewport

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -2,11 +2,13 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-
     @yield('styles')
     @yield('meta')
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="/img/e-fav.gif" type="image/gif">
+
 </head>
 <body>
 <div class="wrapper">
