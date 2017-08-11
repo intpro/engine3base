84d6190c58466753e6d776ba7e83b1cf8b6ff3bc
Kaluchii <mr.kaluchii@mail.ru>
2017-08-11 11:16:29 +0600

    Update scripts.blade.php
    Comment recaptcha script

--- resources/views/front/scripts.blade.php
+++ resources/views/front/scripts.blade.php
@@ -2,5 +2,5 @@
     <script defer src="/js/jquery.min.js"></script>
     <script defer src="/js/jquery.magnific-popup.min.js"></script>
     <script defer src="/js/form.js"></script>
-    <script defer src="https://www.google.com/recaptcha/api.js"></script>
+    {{--<script defer src="https://www.google.com/recaptcha/api.js"></script>--}}
 @endsection
